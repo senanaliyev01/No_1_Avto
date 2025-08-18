744dabfba41a255a9acbb1ac290cc8933acc164a
senanaliyev01 <senan.eliyev.9997@gmail.com>
2025-08-18 13:59:20 +0400

    Prosesler Hazirlanmasi

--- n1avto/n1avto/settings.py
+++ n1avto/n1avto/settings.py
@@ -150,14 +150,14 @@ import os
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    '/var/www/No_1_Avto/n1avto/static',
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = '/var/www/No_1_Avto/n1avto/staticfiles'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/var/www/No_1_Avto/n1avto/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
